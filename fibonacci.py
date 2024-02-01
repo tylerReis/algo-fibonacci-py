f5bdff70509902c9ac8a6a366d38b4bdda6a8f42
tylerReis <tyler.reis.j@outlook.com>
2024-02-01 15:43:25 -0500

    for in method'

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,16 @@
 def fibonacci(n):
-  pass
+  answer_list = []
+  for x in range(n+1):
+    if x == 0: 
+      answer_list.append(0)
+    elif x == 1:
+      answer_list.append(1)
+    else:
+      answer_list.append(answer_list[x-1] + answer_list[x-2])
+    
+  return answer_list[n]
+
+print(fibonacci(5))
+
 
 
