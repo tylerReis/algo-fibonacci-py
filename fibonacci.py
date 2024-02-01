c402c82705c1d4d75c88be7942896798714a93de
tylerReis <tyler.reis.j@outlook.com>
2024-02-01 15:47:15 -0500

    recursive

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,10 @@
 def fibonacci(n):
-  answer_list = []
-  for x in range(n+1):
-    if x == 0: 
-      answer_list.append(0)
-    elif x == 1:
-      answer_list.append(1)
-    else:
-      answer_list.append(answer_list[x-1] + answer_list[x-2])
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  return fibonacci(n-1) + fibonacci(n-2)
     
-  return answer_list[n]
 
 print(fibonacci(5))
 
